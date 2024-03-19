335b3bfe91d8f7a02fefade9ce57634ea80001f2
min <mingyu8820@naver.com>
2024-03-19 20:14:35 +0900

    v1.3 prime number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,8 +1,7 @@
-test = input("input start number: ").split()
+
+start, end = list(map(int, input("input start number").split()))
 #print(int(test[0]),int(test[1]))
 
-start = int(test[0])
-end = int(test[1])
 
 for k in range(start, end+1):
     is_prime_number = True #Change variable name from count to is_prime_number for readability.
