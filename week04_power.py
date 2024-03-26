25beef891c89b327fc60cf304b6a2724dee7815b
min <mingyu8820@naver.com>
2024-03-26 19:15:44 +0900

    v0.2 power

--- week04_power.py
+++ week04_power.py
@@ -1,3 +1,15 @@
+def power(b, e) -> int:
+    """
+    power function
+    :param b: base number
+    :param e: exponent number
+    :return: power Result value
+    """
+    result = 1
+    for i in range(e):
+        result = result*b
+    return result
+
 base, exponent = map(int, input("input base & exponent number").split())
 print(f"{base}^{exponent} = {base**exponent}")
 print(f"{base}^{exponent} = {pow(base, exponent)}")
