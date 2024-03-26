dd526c0db4428c1d20fd86bf2a5ab45dbac29409
min <mingyu8820@naver.com>
2024-03-26 21:39:41 +0900

    v0.3 math

--- week04_math.py
+++ week04_math.py
@@ -16,3 +16,18 @@ def print_fx(fx):
 coefficient = [5,-2,0,6]
 
 print(print_fx(coefficient))
+
+
+x = int(input("input x value:"))
+def cal_fx(fx, x):
+    expo = len(fx) - 1
+    result = 0
+
+    for k in range(len(fx)):
+        coef = fx[k]
+        result = result + coef * x ** expo
+        expo = expo - 1
+
+    return result
+
+print(cal_fx(coefficient,x))
