04d5f39027df025dea433d4961e7781e88ab79b6
min <mingyu8820@naver.com>
2024-03-26 19:09:15 +0900

    v0.0 power

---
+++ week03.py
@@ -0,0 +1,10 @@
+a = 7
+b = a
+a = True
+print(a,b)
+print(id(a), id(b))
+b = 3.14
+print(a,b)
+print(id(a), id(b))
+print(type(a),type(b))
+help("keywords")
---
+++ week04_power.py
@@ -0,0 +1,2 @@
+base, exponent = map(int, input("input base & exponent number").split())
+print(f"{base}^{exponent} = {base**exponent}")
