4019700df5b5d7bc4b09929aeebc5f56cead039c
min <mingyu8820@naver.com>
2024-03-26 19:34:33 +0900

    v0.3 power

--- week04_power.py
+++ week04_power.py
@@ -6,9 +6,23 @@ def power(b, e) -> int:
     :return: power Result value
     """
     result = 1
-    for i in range(e):
-        result = result*b
+    for _ in range(e):2
+        #result  = result*b
     return result
+
+#def square(n) -> int:
+    #return n * n
+#number = int(input())
+
+#for i in range(number+1):
+   # print(square(i))
+#temp = list()
+#for i in range(number+1):
+    #temp.append(i)
+#temp = [i for i in range(number+1)]
+#print(list(map(lambda i: i * i, temp)))
+#print(list(map(square, temp)))
+
 
 base, exponent = map(int, input("input base & exponent number").split())
 print(f"{base}^{exponent} = {base**exponent}")
