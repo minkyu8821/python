016874467e29991568f3b0751d54236abf3d2c50
min <mingyu8820@naver.com>
2024-04-02 19:12:30 +0900

    v0.4 math

--- week05_math.py
+++ week05_math.py
@@ -9,7 +9,7 @@ def print_fx(fx):
         if coef ==0:
             expo = expo - 1
             continue
-        texts = texts + str(coef) + "x^"+ str(expo)+" "
+        texts = texts + f"{coef}x^{expo} "
 
         expo = expo - 1
     return texts
